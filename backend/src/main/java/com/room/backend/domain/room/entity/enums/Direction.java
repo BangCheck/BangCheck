61347460d1cfb334f24fe7772fc3a533d837cb52
hajimeong <ha_01@naver.com>
2026-05-03 17:43:41 +0900

    feat: Rooms의 Direction, BuildingType ENUM에 description 추가

--- backend/src/main/java/com/room/backend/domain/room/entity/enums/Direction.java
+++ backend/src/main/java/com/room/backend/domain/room/entity/enums/Direction.java
@@ -1,9 +1,18 @@
 package com.room.backend.domain.room.entity.enums;
 
+import lombok.Getter;
+
+@Getter
 public enum Direction {
-    EAST,
-    WEST,
-    SOUTH,
-    NORTH,
-    UNKNOWN
+    EAST("동향"),
+    WEST("서향"),
+    SOUTH("남향"),
+    NORTH("북향"),
+    UNKNOWN("모름");
+
+    private final String description;
+
+    Direction(String description) {
+        this.description = description;
+    }
 }
